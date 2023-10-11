3f3124a15dcfd22d67b0e548893d30a702f9b630
unic12341 <146116068+unic12341@users.noreply.github.com>
2023-10-11 15:19:57 +0200

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,5 @@
 import Effect from "./effect.js";
-import * as PIXI from 'pixi.js';
+const PIXI = window.PIXI;
 
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
